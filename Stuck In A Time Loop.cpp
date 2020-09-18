6e9a320e209baac42683c86ca013e3d60e818778
irfansofyana <irfansofyana0305@gmail.com>
2020-09-18 08:48:58 +0700

    Add stuck in a time loop solution

---
+++ Stuck In A Time Loop.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+
+int main(){
+    cin >> n;
+    for (int i = 0 ; i < n; ++i) {
+        cout << i + 1 << " " << "Abracadabra\n";
+    }
+
+    return 0;
+}
